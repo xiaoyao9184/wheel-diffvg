61040a251495ede0c0a31d3c8040afcbf696a5a9
xiaoyao9184 <6614349+xiaoyao9184@users.noreply.github.com>
2026-03-07 10:50:49 +0800

    add github wheel build workflow

---
+++ script/build-whl@py38.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+conda install -y cmake==3.22.1
+
+export DIFFVG_CUDA=1
+export CUDA_HOME=/usr/local/cuda
+export PATH=/usr/local/cuda/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+
+pip install scipy==11.10.1
+
+python setup.py install
+python setup.py bdist_wheel
+
+chmod -R a+rwX dist/
